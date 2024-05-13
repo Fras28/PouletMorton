3c0a267c769d6057170da53c295997429a435785
Fras28 <fselvarolo28@gmail.com>
2024-05-13 13:23:30 -0300

    test modificacion

--- src/Components/Comander/AdminPanel.jsx
+++ src/Components/Comander/AdminPanel.jsx
@@ -20,7 +20,7 @@ export const AdminPanel = () => {
       {usuarioComander?
         <div>
           <div style={{padding:"1rem",display:"flex", justifyContent:"center" ,gap:"1rem"}}>
-          <button className="buttonDash" onClick={() => setPanel("General")}>General</button>
+          <button className="buttonDash" onClick={() => setPanel("General")}>General </button>
           <button className="buttonDash" onClick={() => setPanel("Estadisticas")}>Estadisticas</button>
           <button className="buttonDash" onClick={() => setPanel("Otros")}>Otros</button>
           <ModalGen Child={<Editer />} txtBtn="Editar Producto"  />
